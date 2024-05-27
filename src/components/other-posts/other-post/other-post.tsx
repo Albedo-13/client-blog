d879b739c8bc40a0cf8afff27a9a08661f75d88f
PavelProkopenya <prokopenya.personal@gmail.com>
2024-05-27 16:33:01 +0300

    feat: other posts partial

--- src/components/other-posts/other-post/other-post.tsx
+++ src/components/other-posts/other-post/other-post.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { BlogPost } from "@/types";
+
 import styles from "./other-post.module.scss";
 
-export function OtherPost() {
-  return <div>OtherPost</div>;
+export function OtherPost({ post }: { post: BlogPost }) {
+  return (
+    <Link href="#" className={styles.card}>
+      <Image src={post.image} className={styles.image} alt={post.title} width={350} height={250} />
+      <p className={styles.creds}>
+        By <span>{post.author}</span> | {post.date}
+      </p>
+      <p className={styles.title}>{post.title}</p>
+      <p className={styles.description}>{post.description}</p>
+    </Link>
+  );
 }
--- src/components/other-posts/other-posts.tsx
+++ src/components/other-posts/other-posts.tsx
@@ -1,11 +1,23 @@
+import { POSTS } from "@/data/blog-posts";
+
+import { OtherPost } from "./other-post/other-post";
 import styles from "./other-posts.module.scss";
 
 export function OtherPosts() {
+  // TODO: filter by current post' category
+  const posts = POSTS.slice(0, 3);
+
   return (
     <section>
-      <h2>What to read next</h2>
-      
-      <hr />
+      <div className="container">
+        <h2>What to read next</h2>
+        <div className={styles.wrapper}>
+          {posts.map((post, index) => (
+            <OtherPost key={post.id} post={post} />
+          ))}
+        </div>
+        <hr />
+      </div>
     </section>
-  )
+  );
 }
