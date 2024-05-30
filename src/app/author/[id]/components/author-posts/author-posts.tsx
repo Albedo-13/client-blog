5e946611ca558bf7dda3fb60204aa8a6c5f4a558
Pavel Prokopenya <prokopenya.personal@gmail.com>
2024-05-30 20:10:08 +0300

    fix: variable & import fixes

--- src/app/author/[id]/components/author-posts/author-posts.tsx
+++ src/app/author/[id]/components/author-posts/author-posts.tsx
@@ -1,6 +1,6 @@
+import { PostCard } from "@/components/post-card/post-card";
 import { POSTS } from "@/data/blog-posts";
 
-import { PostCard } from "../post-card/post-card";
 import styles from "./author-posts.module.scss";
 
 type AuthorPosts = {
