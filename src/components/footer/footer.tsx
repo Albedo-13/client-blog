6c60131c85d9e2a4c707c40979d83ad10a3565cc
Pavel Prokopenya <prokopenya.personal@gmail.com>
2024-05-30 20:14:42 +0300

    fix: empty onClick calls

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -19,7 +19,7 @@ export function Footer() {
           <h2 className={styles.title}>Subscribe to our news letter to get latest updates and news</h2>
           <div className={styles.inputWrapper}>
             <input className={styles.input} placeholder="Enter Your Email" type="text" />
-            <Button type="primary">Subscribe</Button>
+            <Button type="primary" onClick={() => {}}>Subscribe</Button>
           </div>
         </div>
         <address className={styles.address}>
