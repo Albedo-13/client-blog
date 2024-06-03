353af9a485179cceae244486c3c18540db5afc7e
Pavel Prokopenya <prokopenya.personal@gmail.com>
2024-06-03 23:08:02 +0300

    feat: pagination

--- src/app/[locale]/blog/components/blog-cards/blog-cards.tsx
+++ src/app/[locale]/blog/components/blog-cards/blog-cards.tsx
@@ -1,29 +1,50 @@
-import { getTranslations } from "next-intl/server";
+"use client";
+
+import { useTranslations } from "next-intl";
+import { useState } from "react";
 
 import { PostCard } from "@/components/post-card/post-card";
+import { POSTS_PER_PAGE } from "@/constants/constants";
 import { POSTS } from "@/data/blog-posts";
 
 import styles from "./blog-cards.module.scss";
 
-export async function BlogCards() {
-  const t = await getTranslations("BlogCards");
+const MIN_PAGE = 1;
+const MAX_PAGE = Math.ceil(POSTS.length / POSTS_PER_PAGE);
 
+export function BlogCards() {
+  const t = useTranslations("BlogCards");
+  const [page, setPage] = useState(MIN_PAGE);
+
+  const handlePrevClick = () => {
+    if (page - 1 < MIN_PAGE) return;
+    setPage(page - 1);
+  };
+
+  const handleNextClick = () => {
+    if (page + 1 > MAX_PAGE) return;
+    setPage(() => page + 1);
+  };
+
+  const filteredPosts = POSTS.slice((page - 1) * POSTS_PER_PAGE, page * POSTS_PER_PAGE);
   return (
     <section className={styles.section}>
       <div className="container">
         <h2 className={styles.title}>{t("allPosts")}</h2>
         <hr className={styles.divider} />
         <div className={styles.cards}>
-          {POSTS.map((postData) => (
+          {filteredPosts.map((postData) => (
             <PostCard key={postData.id} {...postData} />
           ))}
         </div>
 
         <div className={styles.pagination}>
-          <button className={styles.arrow} disabled>
+          <button className={styles.arrow} onClick={handlePrevClick} disabled={page === MIN_PAGE}>
             {t("prev")}
           </button>
-          <button className={styles.arrow}>{t("next")}</button>
+          <button className={styles.arrow} onClick={handleNextClick} disabled={page === MAX_PAGE}>
+            {t("next")}
+          </button>
         </div>
       </div>
     </section>
