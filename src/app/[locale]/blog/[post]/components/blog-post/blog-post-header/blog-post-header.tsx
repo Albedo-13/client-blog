94c2e998fd237486ca92256bbba97609e1db1048
PavelProkopenya <prokopenya.personal@gmail.com>
2024-06-05 13:11:08 +0300

    styles: blog post page adaptation

--- src/app/[locale]/blog/[post]/components/blog-post/blog-post-header/blog-post-header.tsx
+++ src/app/[locale]/blog/[post]/components/blog-post/blog-post-header/blog-post-header.tsx
@@ -15,35 +15,31 @@ export async function BlogPostHeader({ post }: BlogPostHeaderProps) {
 
   return (
     <div>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <div className={styles.authorWrapper}>
-            <Image
-              className={styles.authorImage}
-              src={post.author.image}
-              width={AVATAR_WIDTH}
-              height={AVATAR_HEIGHT}
-              alt="author"
-            />
-            <div>
-              <p className={styles.author}>{post.author.name}</p>
-              <p className={styles.creds}>
-                {t("postedOn")} {post.date}
-              </p>
-            </div>
-          </div>
-          <h1 className={styles.title}>{post.title}</h1>
-          <div className={styles.category}>
-            <Image
-              className={styles.categoryImage}
-              src={post.category.icon}
-              width={ICON_WIDTH}
-              height={ICON_HEIGHT}
-              alt="category"
-            />
-            <p className={styles.categoryText}>{post.category.label}</p>
-          </div>
+      <div className={styles.authorWrapper}>
+        <Image
+          className={styles.authorImage}
+          src={post.author.image}
+          width={AVATAR_WIDTH}
+          height={AVATAR_HEIGHT}
+          alt="author"
+        />
+        <div>
+          <p className={styles.author}>{post.author.name}</p>
+          <p className={styles.creds}>
+            {t("postedOn")} {post.date}
+          </p>
         </div>
+      </div>
+      <h1 className={styles.title}>{post.title}</h1>
+      <div className={styles.category}>
+        <Image
+          className={styles.categoryImage}
+          src={post.category.icon}
+          width={ICON_WIDTH}
+          height={ICON_HEIGHT}
+          alt="category"
+        />
+        <p className={styles.categoryText}>{post.category.label}</p>
       </div>
     </div>
   );
